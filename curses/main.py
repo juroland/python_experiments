f5edf45febb4504a04c821658f6f95c305a9f0d7
Julien Roland <juroland@gmail.com>
2016-02-10 14:17:33 +0100

    clean up

--- curses/main.py
+++ curses/main.py
@@ -10,48 +10,44 @@ class Maze:
     def __init__(self, x_size, y_size):
         self.x_size = x_size
         self.y_size = y_size
+        self.x_begin = 1
+        self.y_begin = 1
+        self.x_end = x_size - 2
+        self.y_end = y_size - 2;
         self.maze = [[0 for y in range(y_size)] for x in range(x_size)]
         self.maze[1][0] = 1
         self.maze[-2][-1] = 1
 
-    def top_neighbors(self, x, y):
+    def get_neighbors(self, x, y, direction):
+        assert (direction[0] == 0 or direction[1] == 0,
+                "directions are up, down, left, or right")
         neighbors = []
-        if x > 1:
-            neighbors.append((x-1, y))
-            if y > 1:
-                neighbors.append((x-1, y-1))
-            if y < self.y_size-2:
-                neighbors.append((x-1, y+1))
+        x += direction[0]
+        y += direction[1]
+        if direction[0] == 0:
+            coordinates = [(x, y), (x-1, y), (x+1, y)]
+        else:
+            coordinates = [(x, y), (x, y-1), (x, y+1)]
+
+        for x, y in coordinates:
+            if (self.x_begin <= x <= self.x_end
+                    and self.y_begin <= y <= self.y_end):
+                neighbors.append((x,y))
+
         return neighbors
 
-    def bottom_neighbors(self, x, y):
-        neighbors = []
-        if x < self.x_size-2:
-            neighbors.append((x+1, y))
-            if y > 1:
-                neighbors.append((x+1, y-1))
-            if y < self.y_size-2:
-                neighbors.append((x+1, y+1))
-        return neighbors
+    def are_neighbors_free(self, neighbors):
+        return all(self.maze[x][y] == 0 for x, y in neighbors)
 
-    def left_neighbors(self, x, y):
+    def get_free_neighbors(self, x, y):
         neighbors = []
-        if y > 1:
-            neighbors.append((x, y-1))
-            if x > 1:
-                neighbors.append((x-1, y-1))
-            if x < self.x_size-2:
-                neighbors.append((x+1, y-1))
-        return neighbors
-
-    def right_neighbors(self, x, y):
-        neighbors = []
-        if y < self.y_size-2:
-            neighbors.append((x, y+1))
-            if x > 1:
-                neighbors.append((x-1, y+1))
-            if x < self.y_size-2:
-                neighbors.append((x+1, y+1))
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for x0, y0 in directions:
+            directed_neighbors = self.get_neighbors(x, y, (x0, y0))
+            directed_neighbors.extend(self.get_neighbors(x, y, (2*x0, 2*y0)))
+            if (directed_neighbors != []
+                    and self.are_neighbors_free(directed_neighbors)):
+                neighbors.append((x+x0, y+y0))
         return neighbors
 
     def count(self, nodes):
@@ -66,69 +62,30 @@ def depth_first_generator(x_size, y_size):
     M = Maze(x_size, y_size)
     M.maze[x_size-2][y_size-2] = 1
     while len(stack) > 0:
-        u = stack[-1]
-        x = u[0]
-        y = u[1]
-        neighbors = []
-        if x-1 >= 1 and M.maze[x-1][y] == 0:
-            neighbors.append((x-1,y))
-        if y-1 >= 1 and M.maze[x][y-1] == 0:
-            neighbors.append((x,y-1))
-        if x+1 < x_size-1 and M.maze[x+1][y] == 0:
-            neighbors.append((x+1,y))
-        if y+1 < y_size-1 and M.maze[x][y+1] == 0:
-            neighbors.append((x,y+1))
-
-        if len(neighbors) == 0:
+        x, y = stack[-1]
+        neighbors = M.get_free_neighbors(x, y)
+        if neighbors == []:
             stack.pop()
-            continue
-
-        random.shuffle(neighbors)
-        for v in neighbors:
-            if v[1] < y:
-                if M.count(M.left_neighbors(x,y)) == 0:
-                    M.maze[v[0]][v[1]] = 1
-                    stack.append(v)
-                else:
-                    M.maze[v[0]][v[1]] = -1
-                break
-            if v[1] > y:
-                if M.count(M.right_neighbors(x,y)) == 0:
-                    M.maze[v[0]][v[1]] = 1
-                    stack.append(v)
-                else:
-                    M.maze[v[0]][v[1]] = -1
-                break
-            if v[0] < x:
-                if M.count(M.top_neighbors(x,y)) == 0:
-                    M.maze[v[0]][v[1]] = 1
-                    stack.append(v)
-                else:
-                    M.maze[v[0]][v[1]] = -1
-                break
-            if v[0] > x:
-                if M.count(M.bottom_neighbors(x,y)) == 0:
-                    M.maze[v[0]][v[1]] = 1
-                    stack.append(v)
-                else:
-                    M.maze[v[0]][v[1]] = -1
-                break
+        else:
+            random.shuffle(neighbors)
+            x, y = neighbors[0]
+            M.maze[x][y] = 1
+            stack.append((x, y))
 
     return M.maze
 
 def main(stdscr):
     stdscr.clear()
     curses.curs_set(False) # Invisible cursor
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLUE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
     stdscr.addstr(0, 0, '{} x {}'.format(curses.LINES, curses.COLS),
-                  curses.color_pair(1)) 
+                  curses.color_pair(1))
     maze = depth_first_generator(curses.LINES-2, curses.COLS-2)
     for x in range(1, curses.LINES-1):
         for y in range(1, curses.COLS-1):
             if maze[x-1][y-1] != 1:
-                stdscr.addstr(x, y, ' ',
-                              curses.color_pair(1) | curses.A_BOLD)
+                stdscr.addstr(x, y, ' ', curses.color_pair(1) | curses.A_BOLD)
 
     stdscr.refresh()
     while True:
